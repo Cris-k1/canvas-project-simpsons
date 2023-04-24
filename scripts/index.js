20e747c3084632ebf5324bb49ecf523ccce3b8df
Francisco Branco <franciscomcb782@gmail.com>
2023-04-24 16:37:54 +0100

    done

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 const player = new Homer(
-  canvas.width / 2 - 35,
+  (canvas.width / 2) - 35,
   canvas.height - 80,
   70,
   80,
