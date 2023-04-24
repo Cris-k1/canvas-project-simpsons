696c07c5969aedc08a469dca40cfd1d674c18592
Cris Kith <cristiankith@gmail.com>
2023-04-24 11:35:13 +0100

    game.js and index.html updated

---
+++ scripts/game.js
@@ -0,0 +1,29 @@
+class Game{
+    constructor(ctx, width, height, homer){
+        this.ctx = ctx;
+        this.width = width;
+        this.height = height;
+        this.homer = homer;
+        this.intervalId = null;
+        this.frames = 0;
+        this.enemies = [];
+    }
+
+    start(){
+        this.intervalId = setInterval(this.update, 10)
+    }
+
+    update = () =>{
+        this.frames++;
+        this.clear();
+        this.homer.newPos();
+        this.homer.draw();
+        this.updateEnemies();
+        this.checkGameOver();
+    }
+
+    stop(){
+        clearInterval(this.intervalId);
+
+    }
+}
