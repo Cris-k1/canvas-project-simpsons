b3c2000086796a58571f3af97b28390d81159d68
Cris Kith <cristiankith@gmail.com>
2023-04-25 13:09:38 +0100

    bonus and audio

--- scripts/bonus.js
+++ scripts/bonus.js
@@ -1,11 +1,14 @@
 
+// need to upload the images 
+
 updateBonus(){
+   
     let originalY = 0;
-    for (let i = 0; i<this.enemies.length; i++){
+    for (let i = 0; i<this.bonus.length; i++){
         // enemies's y ++
-        this.enemies[i].y +=1; 
+        this.bonus[i].y +=1; 
         // enemies' draw function
-        this.enemies[i].draw();
+        this.bonus[i].draw();
     }
     // each 2 seconds, a enemy is updated
     if(this.frames % 200 === 0){
@@ -16,6 +19,55 @@ updateBonus(){
 
         let randomX = Math.floor(Math.random() * width);
 
-        this.enemies.push(new Component(randomX, originalY, width, 40, false, 'brown', this.ctx))
+        this.bonus.push(new Component(randomX, originalY, width, 40, false, 'brown', this.ctx))
+    }
+
+
+}
+
+getBonus(bonus2){
+    return(this.top()<bonus2.bottom()&&
+    this.right()>bonus2.left()&&
+    this.left()<bonus2.right()&&
+    this.bottom()>bonus2.top())
+}
+
+
+checkBonus(){
+    const bonusUp = this.bonus.some((bonus)=>{
+        return this.player.getBonus(bonus);
+    })
+    if(bonusUp){
+    
+    
+        this.ctx.fillText(`${this.score}`);
+       
+    }
+}
+
+
+//score - not sure if it works
+let score = 0;
+function drawScore(){
+    ctx.font = "16px Helvetica";
+    ctx.fillStyle = "#0095DD";
+    ctx.fillText(`Score: ${score}`, 8, 20);
+}
+
+function collisionDetection(){
+    for (let c = 0; c < bonusCount; c++){
+        const b = bonus[c];
+        if (b.status === 1){
+            if (
+                x > b.x &&
+                x < b.x + bonusWidth &&
+                y > b.y &&
+                y < b.y + bonusHeight
+            ){
+                dy = -dy;
+                b.status = 0;
+                score++;
+            }
+        }
     }
 }
